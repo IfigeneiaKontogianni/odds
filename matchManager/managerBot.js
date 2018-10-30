33a2f4ef1b820eb52743d1e48e164dcef894cd75
Alcaind <vmanol09@gmail.com>
2018-10-30 23:21:17 +0200

    match manager prototyping

--- matchManager/managerBot.js
+++ matchManager/managerBot.js
@@ -1,14 +1,12 @@
 const puppeteer = require('puppeteer');
-const socket = require('socket.io-client')('http://91.138.138.18:3000/matchmanagers');
-
-function socketInit() {
-
-}
+const socket = require('socket.io-client')('http://localhost:3000/managers');
 
 async function init() {
     var myJobs = [], jCnt = 0, myName = '007', startingAt = 0;
     var matchClass = '', liveClass = '';
     var pagesToParse = [], parsingClass = '';
+    var runningMatchesInPlay = [];
+    var runningMatchesPreGame = [];
 
 
     socket.on('connect', function () {
@@ -26,7 +24,7 @@ async function init() {
     socket.on('set_name', function (data) {
         myName = data;
     });
-    socket.on('job', function (data) {
+    socket.on('jobs', function (data) {
         execJobs(data);
     });
 
@@ -53,32 +51,82 @@ async function init() {
     });
     socket.emit('give-jobs');
 
-    async function parsePages(pg) {
-        var ret = await pg.evaluate(() => {
+    async function parsePages(pg, matchClass, liveClass) {
+        var ret = await pg.evaluate((matchClass, liveClass) => {
             let domElements = document.getElementsByClassName(matchClass);
             var mainobj = {
-                header: {totalMatches: 0, inPlay: 0, preGame: 0},
-                matches: {inplay: [], preGame: []},
-                changes: {added: [], deleted: []}
+                matches: {inPlay: [], preGame: [], totalMatches: 0, totalInPlay: 0, totalPreGame: 0},
+                changes: {added: {inPlay: [], preGame: []}, deleted: {inPlay: [], preGame: []}}
             };
 
             for (var el of domElements) {
-                mainobj.totalMatches++;
-                mainobj.markets.push(translateObj(el));
+                mainobj.matches.totalMatches++;
+                if (el.getElementsByClassName(liveClass).length > 0) {
+                    mainobj.matches.totalInPlay++;
+                    mainobj.matches.inPlay.push({name: el.innerText.split('\n')[0] + ' v ' + el.innerText.split('\n')[2]});
+                } else {
+                    mainobj.matches.totalPreGame++;
+                    mainobj.matches.preGame.push({name: el.innerText.split('\n')[0]});
+                }
             }
 
             return mainobj;
-        });
+
+        }, matchClass, liveClass);
         return ret;
-    };
+    }
 
-    var matchesPages = [];
+    function makeData(mainobj) {
+        for (let rm of runningMatchesInPlay) rm.old = true;
+        for (let rm of runningMatchesPreGame) rm.old = true;
 
-    setInterval(async () => {
-            for (p of pagesToParse)
-                socket.emit('send-data', await parsePages(p))
-        },
-        Math.random() * 4000);
+        for (let mo of mainobj.matches.inPlay) {
+            console.log(mo.name);
+            if (!runningMatchesInPlay[mo.name]) {
+                mainobj.changes.added.inPlay.push(mo);
+            }
+            runningMatchesInPlay[mo.name] = mo;
+        }
+
+        for (let rm of runningMatchesInPlay) {
+            if (mo.old) {
+                mainobj.changes.deleted.inPlay.push(rm);
+                delete rm;
+            }
+        }
+
+        for (let mo of mainobj.matches.preGame) {
+            if (!runningMatchesPreGame[mo.name]) {
+                mainobj.changes.added.preGame.push(mo);
+            }
+            runningMatchesPreGame[mo.name] = mo;
+        }
+
+        for (let rm of runningMatchesPreGame) {
+            if (mo.old) {
+                mainobj.changes.deleted.preGame.push(rm);
+                delete rm;
+            }
+        }
+
+
+        return mainobj;
+    }
+
+    function startParsing() {
+
+        setInterval(async () => {
+                for (p of pagesToParse) {
+                    // console.log('runningMatchesInPlay  : ', runningMatchesInPlay);
+                    // console.log('sending data ...', tmp);
+                    var mArray = await parsePages(p, matchClass, liveClass);
+                    socket.emit('send-data', makeData(mArray));
+                    console.log('runningMatchesInPlay  : ', runningMatchesInPlay);
+                    console.log('runningMatchesPreGame  : ', runningMatchesPreGame);
+                }
+            },
+            Math.random() * 4000);
+    }
 
     async function execJobs(jobs) {
         myJobs = jobs.cmds;
@@ -91,28 +139,32 @@ async function init() {
             console.log(jCnt, myJobs.length - 1);
             if (jCnt === myJobs.length) {
                 clearInterval(cmdInterv);
+                startParsing();
             }
-        }, Math.random() * 2000 + 14000);
+        }, Math.random() * 2000 + 8000);
     }
 
-    async function execCommand(cmd, pg) {
+    async function execCommand(cmd) {
         socket.emit('bot-exec', cmd);
         console.log('exec command ', cmd);
+        pg = pagesToParse[pagesToParse.length - 1];
         try {
             switch (cmd.a) {
                 case 'np' :
                     let npg = await browser.newPage();
                     await npg.goto(cmd.loc);
-                    await npg.waitFor(8000);
+                    await npg.waitFor(4000);
+                    npg.on('console', consoleObj => console.log(consoleObj.text()));
                     pagesToParse.push(npg);
                     return npg;
                     break;
                 case 'gt' :
                     await pg.goto(cmd.loc);
-                    await pg.waitFor(8000);
+                    await pg.waitFor(4000);
                     break;
                 case 'click' :
                     await pg.click(cmd.loc);
+                    await pg.waitFor(6000);
                     break;
             }
         } catch (error) {
@@ -122,5 +174,6 @@ async function init() {
             return;
         }
     }
-};
+}
+
 init();
